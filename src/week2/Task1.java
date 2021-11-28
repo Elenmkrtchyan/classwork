505fdcd57135297a948be6a194f4f1f150f16445
Elen <elenmkrtchyan97@gmail.com>
2021-11-28 18:02:59 +0400

    day 1

--- src/week2/Task1.java
+++ src/week2/Task1.java
@@ -3,6 +3,7 @@ package week2;
 import java.util.Scanner;
 
 public class Task1 {
+    
     public static void main(String[] args) {
       Scanner scan = new Scanner(System.in) ;
       byte b = scan.nextByte();
